cf5a1424133e877e26f6e88bd88e02e6be932874
serverminecraftangel <100566576+serverminecraftangel@users.noreply.github.com>
2022-10-31 15:44:29 +0100

    Update product.js

--- src/product.js
+++ src/product.js
@@ -2,7 +2,7 @@ import fixText from "./fixtext";
 
 const product = async (query) => {
   const product_page = await (
-    await fetch(`https://www.amazon.in/${query}`)
+    await fetch(`https://www.amazon.it/${query}`)
   ).text();
 
   try {
